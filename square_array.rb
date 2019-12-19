ed3f74b6cd192a7e3bfcde8ce8cf954d37c77470
Emily Wu <huijiawu90@gmail.com>
2019-12-19 04:54:41 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,9 +1,9 @@
 def square_array(array)
-  # # code below is approach using '.each' method only 
-  # squares = []
-  # array.each { |num| squares << num ** 2 }
-  # squares
+  # code below is approach using '.each' method only 
+  squares = []
+  array.each { |num| squares << num ** 2 }
+  squares
   
-  # alternative approach using '.collect' method
-  puts array.map { |num| num * num }
+  # # alternative approach using '.collect' method
+  # array.collect { |num| num * num }
 end
